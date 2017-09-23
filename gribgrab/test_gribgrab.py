d07ae83c1c922dfb0347ddad6f61a3db074a0c4c
Dave Sproson <davesproson@gmail.com>
2017-09-23 13:58:18 +0100

    Added some docstrings to tests.

--- gribgrab/test_gribgrab.py
+++ gribgrab/test_gribgrab.py
@@ -6,6 +6,7 @@ import gribgrab
 class ExistsTestCase(unittest.TestCase):
 
     def test_no_future_data(self):
+        """Check exists() for data in the future returns False"""
         cycle = datetime.datetime.utcnow().now().replace(
             hour=0, minute=0, second=0, microsecond=0
         ) + datetime.timedelta(days=3)
@@ -26,24 +27,31 @@ class IdxFieldTestCase(unittest.TestCase):
         self.idx = gribgrab.IdxField(self.idx_field)
 
     def test_idx_index(self):
+        """Check the idx index number is correctly parsed"""
         self.assertEqual(self.idx.index, 6)
 
     def test_idx_bytes_start(self):
+        """Check the idx byte start is correctly parsed"""
         self.assertEqual(self.idx.bytes_start, 637816)
 
     def test_idx_reftime(self):
+        """Check the idx reference time is correctly parsed"""
         self.assertEqual(self.idx.reftime, datetime.datetime(1995, 10, 30))
 
     def test_idx_varname(self):
+        """Check the idx variable name is correctly parsed"""
         self.assertEqual(self.idx.varname, 'UGRD')
 
     def test_idx_level(self):
+        """Check the idx level is correctly parsed"""
         self.assertEqual(self.idx.level, '30 m above ground')
 
     def test_idx_interval(self):
+        """Check the idx lead time/interval is correctly parsed"""
         self.assertEqual(self.idx.interval, '165 hour fcst')
 
     def test_idx__str__(self):
+        """Check the idx __str__ method is correct"""
         self.assertEqual(str(self.idx), self.idx_field)
 
 
@@ -64,26 +72,31 @@ class IdxCollectionTestCase(unittest.TestCase):
             self.idx_collection.add_idx(idx)
 
     def test_byterange_field_1(self):
+        """Check the byterange for field 1"""
         idx_field = self.idxes[0]
         byterange = self.idx_collection.byterange(idx_field)
         self.assertEqual(byterange, (0, 73572))
 
     def test_byterange_field_2(self):
+        """Check the byterange for field 2"""
         idx_field = self.idxes[1]
         byterange = self.idx_collection.byterange(idx_field)
         self.assertEqual(byterange, (73573, 263117))
 
     def test_byterange_field_3(self):
+        """Check the byterange for field 3"""
         idx_field = self.idxes[2]
         byterange = self.idx_collection.byterange(idx_field)
         self.assertEqual(byterange, (263118, ''))
 
     def test_filter_pres(self):
+        """Check filtering just for the PRES variable"""
         idxes = self.idx_collection.filter(regex='.*PRES.*')
         self.assertEqual(len(idxes), 1)
         self.assertEqual(str(idxes[0]), self.idx_fields[2])
 
     def test_filter_pres_or_gust(self):
+        """Check filtering for PRES or GUST variables"""
         idxes = self.idx_collection.filter(regex='.*(PRES|GUST)+:.*')
         self.assertEqual(len(idxes), 2)
 
